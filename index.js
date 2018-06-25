6f54274983b0c8db0bc96ef9f4df5194ccf0597d
Gilles Piou <gilles.piou@gmail.com>
2018-06-25 19:24:23 +0800

    Setup Prettier (#1)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var _     = require('lodash');
-var map   = require('map-stream');
-var pp    = require('preprocess');
-var path  = require('path');
+var _ = require("lodash");
+var map = require("map-stream");
+var pp = require("preprocess");
+var path = require("path");
 
-module.exports = function (options) {
-  var opts    = _.merge({}, options);
+module.exports = function(options) {
+  var opts = _.merge({}, options);
   var context = _.merge({}, process.env, opts.context);
 
   function ppStream(file, callback) {
@@ -12,15 +12,18 @@ module.exports = function (options) {
 
     // TODO: support streaming files
     if (file.isNull()) return callback(null, file); // pass along
-    if (file.isStream()) return callback(new Error("gulp-preprocess: Streaming not supported"));
+    if (file.isStream())
+      return callback(new Error("gulp-preprocess: Streaming not supported"));
 
     context.src = file.path;
     context.srcDir = opts.includeBase || path.dirname(file.path);
-    context.NODE_ENV = context.NODE_ENV || 'development';
+    context.NODE_ENV = context.NODE_ENV || "development";
 
-    extension = _.isEmpty(opts.extension) ? getExtension(context.src) : opts.extension;
+    extension = _.isEmpty(opts.extension)
+      ? getExtension(context.src)
+      : opts.extension;
 
-    contents = file.contents.toString('utf8');
+    contents = file.contents.toString("utf8");
     contents = pp.preprocess(contents, context, extension);
     file.contents = new Buffer(contents);
 
@@ -31,6 +34,6 @@ module.exports = function (options) {
 };
 
 function getExtension(filename) {
-  var ext = path.extname(filename||'').split('.');
+  var ext = path.extname(filename || "").split(".");
   return ext[ext.length - 1];
 }
